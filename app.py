bc21c65e3f447ad5aafd37d7337982b7a3ce02db
Nan1914 <fernan41938@gmail.com>
2026-01-28 11:13:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -265,8 +265,8 @@ else:
     # ---------------------------------------
     # CUENCAS DE ATRACCIÓN
     # ---------------------------------------
-    elif opcion == "Cuencas de Duffing":
-        st.title("Cuencas del Oscilador de Duffing")
+    elif opcion == "Oscilador de Duffing":
+        st.title("Oscilador de Duffing")
         st.markdown("""
         ### El mapa del Doble Pozo
         Visualizamos la evolución de un sistema con dos estados estables (como dos imanes).
@@ -348,6 +348,7 @@ else:
         """)
         st.latex(r"z_{n+1} = z_n - \frac{f(z_n)}{f'(z_n)}")
         st.info("🚧 Sección en construcción.")
+
 
 
 
